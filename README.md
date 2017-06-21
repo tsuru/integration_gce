fb2801bc5b66986f9a90f357228780190c9da3c0
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-06-21 17:34:55 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Integration test for Tsuru on Google Cloud Engine
+
+This project should make it easier to run Tsuru integration test on Google Cloud Engine.
---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+function abspath() { echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"; }
+mypath=$(abspath $(dirname ${BASH_SOURCE[0]}))
+finalconfigpath=$(mktemp)
+cp ${mypath}/config.yml ${finalconfigpath}
+sed -i.bak "s|\$GCE_PROJECT_ID|${GCE_PROJECT_ID}|g" ${finalconfigpath}
+
+tmpdir=$(mktemp -d)
+export GOPATH=${tmpdir}
+export PATH=$GOPATH/bin:$PATH
+echo "Go get platforms..."
+go get github.com/tsuru/platforms/...
+echo "Go get tsuru..."
+go get github.com/tsuru/tsuru/...
+echo "Go get tsuru client..."
+go get github.com/tsuru/tsuru-client/...
+
+export TSURU_INTEGRATION_examplesdir="${GOPATH}/src/github.com/tsuru/platforms/examples"
+export TSURU_INTEGRATION_installerconfig=${finalconfigpath}
+export TSURU_INTEGRATION_nodeopts="iaas=dockermachine"
+export TSURU_INTEGRATION_maxconcurrency=4
+export TSURU_INTEGRATION_verbose=1
+export TSURU_INTEGRATION_enabled=1
+
+go test -v -timeout 120m github.com/tsuru/tsuru/integration
+
+rm -f ${finalconfigpath}
