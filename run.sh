c08181f32582e55dce8a9faf8a118e803ce7fd6f
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-06-27 17:08:11 -0300

    Allow verbosity level configuration to be predefined

--- run.sh
+++ run.sh
@@ -30,8 +30,10 @@ export TSURU_INTEGRATION_examplesdir="${GOPATH}/src/github.com/tsuru/platforms/e
 export TSURU_INTEGRATION_installerconfig=${finalconfigpath}
 export TSURU_INTEGRATION_nodeopts="iaas=dockermachine"
 export TSURU_INTEGRATION_maxconcurrency=4
-export TSURU_INTEGRATION_verbose=1
 export TSURU_INTEGRATION_enabled=1
+if [ -z $TSURU_INTEGRATION_verbose ]; then
+  export TSURU_INTEGRATION_verbose=1
+fi
 
 go test -v -timeout 120m github.com/tsuru/tsuru/integration
 
