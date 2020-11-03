3025de2b392b796cff48c94b1797c5a1ca0edc0c
Wilson Júnior <wilsonpjunior@gmail.com>
2020-11-03 10:29:03 -0300

    attempt to fix tsuru-client build

--- run.sh
+++ run.sh
@@ -61,10 +61,12 @@ export GOPATH=${tmpdir}
 export PATH=$GOPATH/bin:$PATH
 echo "Go get platforms..."
 go get -d github.com/tsuru/platforms/examples/go
+mkdir -p $GOPATH/src/github.com/tsuru
 echo "Go get tsuru..."
-go get github.com/tsuru/tsuru/integration
-echo "Go get tsuru client..."
-go get -d github.com/tsuru/tsuru-client/tsuru
+pushd $GOPATH/src/github.com/tsuru
+git clone https://github.com/tsuru/tsuru
+git clone https://github.com/tsuru/tsuru-client
+popd
 pushd $GOPATH/src/github.com/tsuru/tsuru-client
 if [ "$TSURUVERSION" != "latest" ]; then
   MINOR=$(echo "$TSURUVERSION" | sed -E 's/^[^0-9]*([0-9]+\.[0-9]+).*$/\1/g')
